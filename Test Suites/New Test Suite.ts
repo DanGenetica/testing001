c66a1b2980eda3fb0ea36779339dbf1042bdfe66
anbinh <anbinh@169.254.227.40>
2021-09-08 23:48:13 +0700

    Test commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>c2bc8f75-372d-4d6d-a70b-4b50bf6c8239</testSuiteGuid>
+   <testCaseLink>
+      <guid>3999db27-5d81-43d5-a968-9e7f4695e373</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testcase1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e4f64ebc-1fa3-4e0b-84e9-3cbcd3439763</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Testcase2</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
